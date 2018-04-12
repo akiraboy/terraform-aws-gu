8dc5d2bcb493eec0657dae8a6ca8c84712084152
tobi <tmszbiernat@gmail.com>
2018-04-12 10:57:14 +0200

    deleting the vpc file

--- vpc.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "vpc" {
-  cidr_block           = "${var.vpc_cidr}"
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-
-  tags {
-    Name = "${var.env_name}-vpc"
-  }
-}
